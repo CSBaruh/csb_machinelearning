6704a0345ba74af2d1316cfbec473a7c590b004a
CSBaruh <cemsbaruh@hotmail.com>
2025-04-05 23:55:20 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,6 +65,6 @@ with st.expander('Input features'):
 
 # Encode
 
-#encode = ['product_name', 'payment_method']
-df_sales = pd.get_dummies(input_sales, prefix=['product_name', 'payment_method'])
+encode = ['product_name']
+df_sales = pd.get_dummies(input_sales, prefix=encode)
 df_sales
