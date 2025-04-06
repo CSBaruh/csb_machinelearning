c6d3feaa8c1a34de8c59c5e4ee0a1179a7ec433f
CSBaruh <cemsbaruh@hotmail.com>
2025-04-06 23:34:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -116,13 +116,12 @@ prediction_proba = clf.predict_proba(input_row)
 #prediction_proba.columns = ['Electronics', 'Sports & Outdoors', 'Books & Stationery', 'Fashion', 'Home & Living']
 
 df_prediction_proba = pd.DataFrame(prediction_proba)
-df_prediction_proba.colmn = ['Electronics', 'Sports & Outdoors', 'Books & Stationery', 'Fashion', 'Home & Living']
-df_prediction_proba.rename(columns= {0: 'Electronics', 
+df_prediction_proba.colmns = ['Electronics', 'Sports & Outdoors', 'Books & Stationery', 'Fashion', 'Home & Living']
+df_prediction_proba.rename(columns={0: 'Electronics', 
                            1: 'Sports & Outdoors', 
                            2: 'Books & Stationery', 
                            3: 'Fashion', 
-                           4: 'Home & Living'}
-                          )
+                           4: 'Home & Living'})
 df_prediction_proba
 
 
