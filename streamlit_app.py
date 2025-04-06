b1bda119f921abf433993da1b80c95fb4ef9462c
CSBaruh <cemsbaruh@hotmail.com>
2025-04-06 23:02:27 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
 st.title('🌐 Machine Learning App')
 
@@ -101,5 +102,22 @@ with st.expander('Data preparation'):
   input_row
   st.write('**Encoded y**')
   y
+
+#  Model trainig and inference
+## Train ML model
+clf = RandomForestClassifier()
+clf.fit(X_raw,y)
+
+## Apply model to make predictions
+predicition = clf.predict(input_row)
+prediction_proba = clf.predict_proba(input_row)
+
+
+
+
+
+
+
+
 
 
